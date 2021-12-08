0cc373aaa1664a6a7e969384a817a6e8e11b9a69
spencerdooley00 <51540623+spencerdooley00@users.noreply.github.com>
2021-12-08 10:48:23 -0500

    uploading data and code files

--- test_Project3.py
+++ test_Project3.py
@@ -2,6 +2,8 @@
 from filter_ssvep_data import make_bandpass_filter
 from filter_ssvep_data import filter_data
 import Project3
+import remove_audvis_blinks
+import numpy as np
 
 fif_file, raw_eeg_data, eeg_times, channel_names, fs = Project3.load_data('13')
 
@@ -9,24 +11,20 @@ start_time = 0
 end_time = 1.5
 
 
-
 eeg_epochs, epoch_times, target_events, all_trials, event_stimulus_ids = Project3.get_eeg_epochs(fif_file, raw_eeg_data, start_time, end_time, fs)
 
 is_target_event = Project3.get_event_truth_labels(all_trials)
 
-tempo_labels = Project3.get_tempo_labels(event_stimulus_ids)
-
-
 eeg_epochs_fft, fft_frequencies = Project3.get_frequency_spectrum(eeg_epochs, fs)
 
-
 channels_to_plot = ['Fz']
-
 
 Project3.plot_power_spectrum(eeg_epochs_fft, fft_frequencies, is_target_event, channels_to_plot, channel_names)
 
 mean_eeg, rms_eeg, std_eeg = Project3.extract_eeg_features(eeg_epochs)
 
 mixing_matrix = Project3.perform_ICA(fif_file, channel_names)
+
+remove_audvis_blinks.plot_components(mixing_matrix, channel_names, components_to_plot=np.arange(0,10,1))
 
 # %%
